02546eb76fdd1883af08ab43ec966b1d86c4dd84
nicolasrsande <nicolasrsande@gmail.com>
2020-05-17 23:35:00 -0300

    fix require

--- lib/rails_pmc.rb
+++ lib/rails_pmc.rb
@@ -2,6 +2,9 @@ require 'fixy'
 require 'rails_pmc/version'
 require 'rails_pmc/documents/envio_facturas'
 require 'rails_pmc/fixy/formatter/numeric'
+require 'rails_pmc/records/envio_facturas_header'
+require 'rails_pmc/records/envio_facturas_detalle'
+require 'rails_pmc/records/envio_facturas_trailer'
 
 module RailsPmc
   class Error < StandardError; end
